7641078b0242e8aa42f8d985302048ec450ee124
Betty <ritamutyaba@gmail.com>
2015-11-17 14:18:52 -0800

    Inserted missing code

--- app/views.py
+++ app/views.py
@@ -3,10 +3,31 @@ from app import app
 from .forms import LoginForm
 
 # index view function suppressed for brevity
-
+@app.route('/')
+@app.route('/index')
+def index():
+     return "Hello World!"
+     user = {'nickname': 'Miguel'} # fake user
+     posts= [ # fake array of posts
+            {
+                'author': {'nickname': 'John'},
+                'body': 'Beautiful day in Portland!'
+            },
+            {
+                'author': {'nickname': 'Susan'},
+                'body': 'The Avenges movie was so cool!'
+            }
+        ]
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html',
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember-me=%s' %
+                (form.openid,data, str(form.remember_,me.data)))
+        return redirect('/index')
+    return render_template( 'index.html',
+                            'login.html',
                             title='Sign In',
+                            user=user,
+                            posts=posts,
                             form=form)
